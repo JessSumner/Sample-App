cd2987092524bd6eacd3f0d4cb955d9ab0962c34
Jessica Sumner <jess.r.sumner@gmail.com>
2015-08-10 20:56:52 -0400

    Configured emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,4 +14,18 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+  # Sending emails
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'aqueous-ridge-3321.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
